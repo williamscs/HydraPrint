d6db72de7f06fd2ad2cd7fa26699899393a5b0ed
Chris Williams <cwilliams88@gmail.com>
2015-03-29 13:24:09 -0700

    Get started with chartjs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+var chartTool = {
+	options: undefined,
+	data: {},
+	populateData: function(data) {
+		this.data = {
+						labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+						datasets: [
+							{
+								label: 'My Second dataset',
+								fillColor: 'rgba(151,187,205,0.2)',
+								strokeColor: 'rgba(151,187,205,1)',
+								pointColor: 'rgba(151,187,205,1)',
+								pointStrokeColor: '#fff',
+								pointHighlightFill: '#fff',
+								pointHighlightStroke: 'rgba(151,187,205,1)',
+								data: [28, 48, 40, 19, 86, 27, 90]
+							}
+						]
+					};
+	},
+	updateDisplay: function() { 
+		// Get context with jQuery - using jQuery's .get() method.
+		var ctx = $('#myChart').get(0).getContext('2d');
+		// This will get the first returned node in the jQuery collection.
+		var myNewChart = new Chart(ctx);
+		var myLineChart = myNewChart.Line(this.data);
+	}
+
+};
+
+chartTool.populateData();
+chartTool.updateDisplay();
