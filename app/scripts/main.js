a0299f16802b7d3de5020a5fbd48fa72c1c244cc
Chris Williams <cwilliams88@gmail.com>
2015-03-30 07:59:36 -0700

    HSL to RGB(A)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,16 +10,17 @@ var chartTool = {
 	},
 	generateDataset: function(label) {
 		var newObj = {};
+		var rgbString = this.hslToRgb(this.currHSL / 360, 0.8, 0.7);
+
 
 		newObj.label = label;
-		newObj.fillColor = 'hsl(' + this.currHSL + ', 30%, 90%)';
-		newObj.strokeColor = 'hsl(' + this.currHSL + ', 80%, 50%)';
-		newObj.pointColor = 'hsl(' + this.currHSL + ', 80%, 50%)';
-		newObj.pointHighlightStroke = 'hsl(' + this.currHSL + ', 80%, 50%)';
+		newObj.fillColor = 'rgba(' + rgbString + ', 0.2)';
+		newObj.strokeColor = 'rgba(' + rgbString + ', 1)';
+		newObj.pointColor = 'rgba(' + rgbString + ', 1)';
+		newObj.pointHighlightStroke = 'rgba(' + rgbString + ', 1)';
 		newObj.pointStrokeColor = '#fff';
 		newObj.pointHighlightFill = '#fff';
 		newObj.data = [];
-		console.log(JSON.stringify(newObj));
 
 		this.currHSL = this.genHSL(this.currHSL);
 		return newObj;
@@ -69,6 +70,43 @@ var chartTool = {
 	genHSL: function(currentHSL) {
 		//137.5
 		return (currentHSL + 137.5) % 360;
+	},
+	/**
+	 * http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+	*/
+	hslToRgb: function(h, s, l){
+		var r, g, b;
+
+		if(s === 0){
+			r = g = b = l; // achromatic
+		}else{
+			var hue2rgb = function hue2rgb(p, q, t){
+				if(t < 0){
+					t += 1;
+				}
+				if(t > 1){
+					t -= 1;
+				}
+				if(t < 1/6){
+					return p + (q - p) * 6 * t;
+				}
+				if(t < 1/2){
+					return q;
+				}
+				if(t < 2/3){
+					return p + (q - p) * (2/3 - t) * 6;	
+				}
+				return p;
+			};
+
+			var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			var p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1/3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1/3);
+		}
+
+	    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)].join(',');
 	},
 	toTitleCase: function(str) {
 		return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}); //from stackoverflow
@@ -141,9 +179,39 @@ var sampleJson2 = {
 	'materials': {}
 };
 
+var sampleJson3 = {
+	'state': 'ready',
+	'tools': {
+		'bed': {
+			'type': 'bed'
+		},
+		'extruder': {
+			'type': 'extruder'
+		}
+	},
+	'sensors': {
+		'bed': {
+			'type': 'temperature',
+			'description': 'bed temperature sensor',
+			'temperature': 60
+		},
+		'extruder': {
+			'type': 'temperature',
+			'description': 'extruder temperature sensor',
+			'temperature': 90
+		}
+	},
+	'materials': {}
+};
+
 chartTool.populateData(sampleJson);
 chartTool.updateDisplay();
 setTimeout(function () {
     chartTool.populateData(sampleJson2);
 	chartTool.updateDisplay();
 }, 5000);
+
+setTimeout(function () {
+    chartTool.populateData(sampleJson3);
+	chartTool.updateDisplay();
+}, 10000);
